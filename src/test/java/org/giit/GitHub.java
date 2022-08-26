118b4a6c43d7d2037f64763ed4e030d095d003da
nandha1471 <nandha.backup.ac@gmail.com>
2022-08-27 01:47:48 +0530

    updating cloning steps

--- src/test/java/org/giit/GitHub.java
+++ src/test/java/org/giit/GitHub.java
@@ -14,8 +14,15 @@ public class GitHub {
 			+         " git remote remove origin"
 			+         " git remote add origin https://token@github.com/username/repositoryname.git"
 			+         " git push -u origin master";
-	
-
 	}
+	private void gitCloning() {
 
+		String gitclone = " Create a folder in local or desktop"
+				+         " BASH HERE Using Right Click"
+				+         " git clone URL from repository"
+				+         " In eclipse file->import->select the folder created--> finish"
+				+         " the clone will be imported";
+
+
+}
 }
